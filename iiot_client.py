7831a00a18961065dba709c3c4ac653afd7bec7d
Vitaliy Safonov <safonov.vitvrn@gmail.com>
2020-05-12 13:30:31 +0300

    fix

--- iiot_client.py
+++ iiot_client.py
@@ -5,9 +5,9 @@ import iiot_device_pb2
 import iiot_device_pb2_grpc
 
 
-print("I'm working here!")
+print("Before run")
 def run():
-    with grpc.insecure_channel('localhost:8080') as channel:
+    with grpc.insecure_channel('localhost:7654') as channel:
     	stub = iiot_device_pb2_grpc.IoTDeviceStub(channel)
-    	response = stub.getDeviceList(iiot_device_pb2.DeviceListRequest(token=''))
-    print("It works! " + response.message)
+    	response = stub.getDeviceList(iiot_device_pb2.DeviceListRequest())
+    print("response: " + response.message)
